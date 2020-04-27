76c7c9475f8acb115704dff8d1bc7d4e61b1c470
dirigne <dirigne@miamioh.edu>
2020-04-27 16:20:27 -0400

    Update classpath

--- gfsh_init.sh
+++ gfsh_init.sh
@@ -1,6 +1,6 @@
 #!/bin/gfsh
 
-start locator --classpath=/pwd
-start server --classpath=/pwd
+start locator --classpath=/pwd/out/production/classes
+start server --classpath=/pwd/out/production/classes
 create region --name=stocks --type=REPLICATE
 create region --name=companies --type=REPLICATE
